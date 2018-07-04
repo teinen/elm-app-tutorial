c6f38d647a2d1fa96010bba565b81ba299929037
諦念 <teinen.github@gmail.com>
2018-07-05 08:54:06 +0900

    Update main messages

--- src/Messages.elm
+++ src/Messages.elm
@@ -1,8 +1,10 @@
 module Messages exposing (..)
 
+import Navigation exposing (Location)
 import Players.Messages
 
 
 -- MESSAGE
 type Msg
     = PlayersMsg Players.Messages.Msg
+    | OnLocationChange Location
